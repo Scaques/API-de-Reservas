9a7f9a6dc2524b80ec19d7668d0d48f61491f1f0
Scaques <112453896+Scaques@users.noreply.github.com>
2026-06-04 21:21:29 -0400

    Add files via upload

---
+++ reservas-api/src/test/java/com/portafolio/reservas/ReservasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
