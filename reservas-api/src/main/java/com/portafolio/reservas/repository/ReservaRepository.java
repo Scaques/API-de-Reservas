3df96f4a90eec431e85a903266816451dcbe268b
Scaques <112453896+Scaques@users.noreply.github.com>
2026-06-04 21:22:56 -0400

    Delete reservas-api directory

--- reservas-api/src/main/java/com/portafolio/reservas/repository/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.portafolio.reservas.repository;
-
-import com.portafolio.reservas.model.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
